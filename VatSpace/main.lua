92842a7571e22bbb5dd37f9779619582e9f1929a
komuher <komuher@gmail.com>
2017-12-09 04:36:32 +0100

    add basic sounds

--- VatSpace/main.lua
+++ VatSpace/main.lua
@@ -4,14 +4,24 @@
 --
 -----------------------------------------------------------------------------------------
 asteroids = {}
-money = 0 
+money = 0
 moneyText = display.newText( 0, 0, 15, native.systemFont, 16 )
 moneyText.anchorX = 0
+local coin_sound = audio.loadSound( "coin-drop-1.wav" )
+local end_game_sound = audio.loadSound( "endgame_sound.wav" )
+local fail_click = audio.loadSound( "beep17.wav" )
 
 local function tapAsteroid(event)
     if ( event.phase == "ended" ) then
-        money = money + 1000
-        moneyText.text = money
+        asteroid = event.target
+        if asteroid.moneyAmount > 0 then
+            asteroid.moneyAmount = asteroid.moneyAmount - 1
+            money = money + 1000
+            moneyText.text = money
+            audio.play(coin_sound)
+        else
+            audio.play(fail_click)
+        end
     end
     return true
 end
@@ -19,24 +29,22 @@ end
 local function randomizeFieldsFor(asteroid)
     asteroid.speed = math.random() + math.random(2, 5)
     asteroid.moneyAmount = math.random(1, 5)
-    asteroid.body.x = math.random(display.contentWidth + 50, display.contentWidth + 200)
-    asteroid.body.y = math.random(20, display.contentHeight-50)
+    asteroid.x = math.random(display.contentWidth + 50, display.contentWidth + 200)
+    asteroid.y = math.random(20, display.contentHeight-50)
 end
 
 local function moveAsteroid(asteroid)
-    asteroid.body.x = asteroid.body.x - asteroid.speed    
+    asteroid.x = asteroid.x - asteroid.speed
 end
 
 local function createAsteroids()
     for i=1,10 do
-        asteroid = { 
-            body = display.newImage( "green.png"),        
-            speed = 0,
-            moneyAmount = 0
-        }
+        asteroid = display.newImage( "green.png")
+        asteroid.speed = 0
+        asteroid.moneyAmount = 0
         randomizeFieldsFor(asteroid)
-        asteroid.body:scale(2, 2)
-        asteroid.body:addEventListener( "touch", tapAsteroid )        
+        asteroid:scale(2, 2)
+        asteroid:addEventListener( "touch", tapAsteroid )
         asteroids[i] = asteroid
     end
 end
@@ -45,7 +53,7 @@ local function mainListener( event )
     for i=1,#asteroids do
         a = asteroids[i]
         moveAsteroid(a)
-        if a.body.x < -50 then
+        if a.x < -50 then
             randomizeFieldsFor(a)
         end
     end

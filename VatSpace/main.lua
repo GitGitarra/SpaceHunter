26881e398859d132ae4e35c6961241d86d2ec18c
monika.plocica <monika.plocica@gmail.com>
2017-12-09 04:11:17 +0100

    Add createBackground function

--- VatSpace/main.lua
+++ VatSpace/main.lua
@@ -5,30 +5,32 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
-local sheetOptions =
-{
-    width = 500,
-    height = 281,
-    numFrames = 18
-}
-local space_background = graphics.newImageSheet( "space.png", sheetOptions )
 
--- sequences table
-local sequences_spaceStars = {
-    -- consecutive frames sequence
+local function createBackground()
+
+    local sheetOptions =
     {
-        name = "background",
-        start = 1,
-        count = 18,
-        time = 2000,
-        loopCount = 0,
-        loopDirection = "bounce"
+        width = 500,
+        height = 281,
+        numFrames = 18
     }
-}
+    local space_background = graphics.newImageSheet( "space.png", sheetOptions )
 
+    -- sequences table
+    local sequences_spaceStars = {
+        -- consecutive frames sequence
+        {
+            name = "background",
+            start = 1,
+            count = 18,
+            time = 2000,
+            loopCount = 0,
+            loopDirection = "bounce"
+        }
+    }
 
+    local spaceStars = display.newSprite( space_background, sequences_spaceStars )
 
-local spaceStars = display.newSprite( space_background, sequences_spaceStars )
-
-spaceStars:scale(2.5,2.5)
-spaceStars:play()
+    spaceStars:scale(2.5,2.5)
+    spaceStars:play()
+end

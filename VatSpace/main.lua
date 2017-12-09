becf7cc8ab9af8c5dd245bf3ee4563582b714c34
Stefan Kania <stefankania95@gmail.com>
2017-12-09 06:14:52 +0100

    Add coins

--- VatSpace/main.lua
+++ VatSpace/main.lua
@@ -5,11 +5,21 @@
 -----------------------------------------------------------------------------------------
 asteroids = {}
 money = 0
-moneyText = display.newText( 0, 0, 15, native.systemFont, 16 )
-moneyText.anchorX = 0
 local coin_sound = audio.loadSound( "coin-drop-1.wav" )
 local end_game_sound = audio.loadSound( "endgame_sound.wav" )
 local fail_click = audio.loadSound( "beep17.wav" )
+
+local function createCoin(x, y)
+    local coin = display.newImage("coin.png", x, y)
+    coin:scale(0.2, 0.2)
+    timer.performWithDelay(35, function(event)
+        coin.y = coin.y - 1
+        if event.count == 10 then
+            coin:removeSelf()
+            coin=nil
+        end
+    end, 10)
+end
 
 local function tapAsteroid(event)
     if ( event.phase == "ended" ) then
@@ -17,8 +27,9 @@ local function tapAsteroid(event)
         if asteroid.moneyAmount > 0 then
             asteroid.moneyAmount = asteroid.moneyAmount - 1
             money = money + 1000
-            moneyText.text = money
+            moneyText.text = "Money: " .. money
             audio.play(coin_sound)
+            createCoin(asteroid.x, asteroid.y - 25)
         else
             audio.play(fail_click)
         end
@@ -37,14 +48,24 @@ local function moveAsteroid(asteroid)
     asteroid.x = asteroid.x - asteroid.speed
 end
 
-local function createAsteroids()
-    for i=1,10 do
+local function createCoinAsteroids()
+    for i=10,15 do
         asteroid = display.newImage( "green.png")
         asteroid.speed = 0
         asteroid.moneyAmount = 0
         randomizeFieldsFor(asteroid)
         asteroid:scale(2, 2)
         asteroid:addEventListener( "touch", tapAsteroid )
+        asteroids[i] = asteroid
+    end
+end
+
+local function createNormalAsteroids()
+    for i=1,10 do
+        asteroid = display.newImage( "red.png")
+        asteroid.speed = 0
+        randomizeFieldsFor(asteroid)
+        asteroid:scale(2, 2)
         asteroids[i] = asteroid
     end
 end
@@ -59,11 +80,7 @@ local function mainListener( event )
     end
 end
 
-
--- Your code here
-
 local function createBackground()
-
     local sheetOptions =
     {
         width = 500,
@@ -71,10 +88,7 @@ local function createBackground()
         numFrames = 18
     }
     local space_background = graphics.newImageSheet( "space.png", sheetOptions )
-
-    -- sequences table
     local sequences_spaceStars = {
-        -- consecutive frames sequence
         {
             name = "background",
             start = 1,
@@ -84,16 +98,23 @@ local function createBackground()
             loopDirection = "bounce"
         }
     }
-
     local spaceStars = display.newSprite( space_background, sequences_spaceStars )
-
     spaceStars:scale(2.5,2.5)
     spaceStars:play()
 end
 
+local function createMoneyText()
+    moneyText = display.newRect(0, 0, 50, 50)
+    moneyText:setFillColor( 0.5 )
+    moneyText = display.newText( "Money: 0", 0, 15, native.systemFont, 16 )
+    moneyText.anchorX = 0
+end
+
 local function run()
     createBackground()
-    createAsteroids()
+    createNormalAsteroids()
+    createCoinAsteroids()
+    createMoneyText()    
     Runtime:addEventListener( "enterFrame", mainListener )
 end
 

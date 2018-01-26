7103f3d7efa7d265d847114612328b9fc750c53d
monika.plocica <monika.plocica@gmail.com>
2018-01-26 15:41:17 +0100

    Add cats module with tap cat event

---
+++ VatSpace/cats.lua
@@ -0,0 +1,65 @@
+-- cats module
+local sounds = require( "sounds" )
+local ui = require( "ui" )
+local gv = require( "gamevariables" )
+
+local M = {}
+
+M.cats = {}
+
+local function moveCat(cat)
+    cat.x = cat.x - cat.speed
+end
+
+
+local function tapCatInSpace(event)
+    if ( event.phase == "ended" ) then
+        gv.time = gv.time + 10
+        print(gv.time)
+    end
+    return true
+end
+
+local function randomizeFieldsFor(cat)
+    cat.speed = math.random() + math.random(2, 5)
+    cat.moneyAmount = math.random(1, 5)
+    cat.x = math.random(display.contentWidth + 50, display.contentWidth + 350)
+    cat.y = math.random(50, display.contentHeight-60)
+end
+
+local function createCatInSpace()
+    for i=1,1 do
+        cat = display.newImage("resources/graphics/cat.png")
+        cat.speed = 10
+        scaleRandom = math.random(1, 1.5)
+        cat:scale(scaleRandom, scaleRandom)
+        cat.x = math.random(display.contentWidth + 50, display.contentWidth + 350)
+        cat.y = math.random(50, display.contentHeight-60)
+        cat:addEventListener( "touch", tapCatInSpace )
+        cat.id = "cat"
+        M.cats[i] = cat
+    end
+end
+
+function M.move()
+    for i=1,#M.cats do
+        a = M.cats[i]
+        moveCat(a)
+        if a.x < -50 then
+            randomizeFieldsFor(a)
+        end
+    end
+end
+
+function M.create(g)
+    createCatInSpace()
+end
+
+function M.destroy()
+    for i=1,#M.cats do
+        M.cats[i]:removeSelf()
+        M.cats[i] = nil
+    end
+end
+
+return M

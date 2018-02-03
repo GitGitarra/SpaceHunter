3607e7060171274d349670d7cbf394140e66d933
monika.plocica <monika.plocica@gmail.com>
2018-02-03 22:17:32 +0100

    Add cat sound

--- VatSpace/sounds.lua
+++ VatSpace/sounds.lua
@@ -3,6 +3,7 @@ local M = {}
 local coin_sound = audio.loadSound( "resources/sounds/coin-drop-1.wav" )
 local end_game_sound = audio.loadSound( "resources/sounds/endgame_sound.wav" )
 local fail_click = audio.loadSound( "resources/sounds/beep17.wav" )
+local cat_meow = audio.loadSound("resources/sounds/beep17.wav")
 
 function M.playCoinSound()
     audio.play(coin_sound)
@@ -14,6 +15,10 @@ end
 
 function M.playFailClickSound()
     audio.play(fail_click)
+end
+
+function M.playCatMeow()
+    audio.play(cat_meow)
 end
 
 return M

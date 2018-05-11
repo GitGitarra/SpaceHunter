835c1d3cdac059846b77ca5654697f9de0f52956
monika.plocica <monika.plocica@gmail.com>
2018-05-11 20:07:39 +0200

    Add bubble listner and second bubble

--- VatSpace/introdialog.lua
+++ VatSpace/introdialog.lua
@@ -1,12 +1,13 @@
 local M = {}
 
-function M.playDialog()
-    local sheetOptions =
-    {
-        width = 972,
-        height = 180,
-        numFrames = 37
-    }
+local function bubbleListner(event)
+    if (event.name == "bubble1") then 
+        print("to działa!")
+    end
+end
+
+local function dialog1()
+    local sheetOptions = { width = 972, height = 180, numFrames = 37 }
     local speech_bubble = graphics.newImageSheet("resources/graphics/dialog1.png", sheetOptions)
     local sequences_speech = {
         {
@@ -15,18 +16,52 @@ function M.playDialog()
             count = 37,
             time = 3000,
             loopCount = 1,
-            loopDirection = "forward"
+            loopDirection = "forward",
+            onEvent = bubbleListner
         }
     }
-    dialog1 = display.newSprite(speech_bubble, sequences_speech)
+    local dialog1 = display.newSprite(speech_bubble, sequences_speech)
     dialog1.x = 150
     dialog1.y = 150
     dialog1:scale(0.25, 0.25)
+    dialog1:addEventListener( "bubble1", bubbleListner)
     dialog1:play()
+    local event = { name="bubble1", target=dialog1 }
+    dialog1:dispatchEvent( event )
+    
     timer.performWithDelay(4500, function(event)
-    dialog1:removeSelf()
-    dialog1 = nil
+        dialog1:removeSelf()
+        dialog1 = nil
     end, 1)
+end
+
+local function dialog2()
+    local sheetOptions = { width = 954, height = 225, numFrames = 54 }
+    local speech_bubble = graphics.newImageSheet("resources/graphics/dialog2.png", sheetOptions)
+    local sequences_speech = {
+        {
+            name = "bubble1",
+            start = 1,
+            count = 54,
+            time = 3000,
+            loopCount = 1,
+            loopDirection = "forward"
+        }
+    }
+    dialog2 = display.newSprite(speech_bubble, sequences_speech)
+    dialog2.x = 150
+    dialog2.y = 150
+    dialog2:scale(0.25, 0.25)
+    dialog2:play()
+    
+    timer.performWithDelay(4500, function(event)
+        dialog2:removeSelf()
+        dialog2 = nil
+    end, 1)
+end
+
+function M.playDialog()
+    local tm = timer.performWithDelay( 3000, dialog1)
 end
 
 return M

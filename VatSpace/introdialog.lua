2b50e1da0abc06a75e497efed27a85c486894ab0
monika.plocica <monika.plocica@gmail.com>
2018-05-15 11:51:04 +0200

    Remove event listener

--- VatSpace/introdialog.lua
+++ VatSpace/introdialog.lua
@@ -1,11 +1,5 @@
 local M = {}
 
-local function bubbleListner(event)
-    if (event.name == "bubble1") then 
-        print("to działa!")
-    end
-end
-
 local function dialog1()
     local sheetOptions = { width = 972, height = 180, numFrames = 37 }
     local speech_bubble = graphics.newImageSheet("resources/graphics/dialog1.png", sheetOptions)
@@ -24,10 +18,7 @@ local function dialog1()
     dialog1.x = 150
     dialog1.y = 150
     dialog1:scale(0.25, 0.25)
-    dialog1:addEventListener( "bubble1", bubbleListner)
     dialog1:play()
-    local event = { name="bubble1", target=dialog1 }
-    dialog1:dispatchEvent( event )
     
     timer.performWithDelay(4500, function(event)
         dialog1:removeSelf()
@@ -61,7 +52,8 @@ local function dialog2()
 end
 
 function M.playDialog()
-    local tm = timer.performWithDelay( 3000, dialog1)
+    timer.performWithDelay( 3000, dialog1)
+    timer.performWithDelay(7000, dialog2)
 end
 
 return M
